02ea3bfd39c1b5196ec00d00a3463d5fcea793ba
Alex Lamb <alexlamb104@gmail.com>
2022-03-01 15:54:44 -0700

    Completed update

--- server/controller.js
+++ server/controller.js
@@ -12,15 +12,36 @@ module.exports = {
         res.status(200).send(houses);
     },
     createHouse: (req, res) => {
-       let newHouse = req.body;
-       newHouse.id = houseID;
 
-       houses.push(newHouse)
-       res.status(200).send(houses)
-       houseID += 1;
+        if(!req.body.address || !req.body.price || !req.body.imageURL){
+            res.status(400).send(`Must enter each box`)
+        }else{
+            let newHouse = req.body;
+            newHouse.id = houseID;
+     
+            houses.push(newHouse)
+            res.status(200).send(houses)
+            houseID += 1;
+        }
         
     },
     updateHouse: (req, res) => {
+        let {id} = req.params;
+        let {type} = req.body;
+
+        let index = houses.findIndex(house => +house.id === +id);
+        
+        if(houses[index].price < 10000 && type === 'minus') {
+            res.status(400).send(`Price cannot go below 0`)
+        }else if(type === 'minus'){
+            houses[index].price -= 10000;
+            res.status(200).send(houses);
+        } else if(type === 'plus'){
+            houses[index].price += 10000;
+            res.status(200).send(houses);
+        }else{
+            res.sendStatus(400)
+        }
 
     },
 }
